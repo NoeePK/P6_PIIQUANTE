bae5dbef35018108c6f5fdd7e42066807e25dfd2
Noee Pisano <noeepisanokoffel@gmail.com>
2022-11-04 11:08:01 +0100

    wip : strong mdp

--- back/models/user.js
+++ back/models/user.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-const isValidPassword = require('mongoose-custom-validators');
-
-const validatePassword = [
-    isValidPassword({
-        validator: isValidPassword,
-        message: "Votre mot de passe doit contenir : 1 majuscule, 1 minuscule, 1 nombre et 1 caractère spécial."
-    })
-]
+// const isValidPassword = require('mongoose-custom-validators');
 
 // Créer un schema utilisateur
 const userSchema = mongoose.Schema({
-    email: { type: String, require: true, unique: true },
-    password: { type: String, required: true, validate: validatePassword }
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        // validate: {
+        //     validator: isValidPassword,
+        //     message: "Votre mot de passe doit contenir : 1 majuscule, 1 minuscule, 1 nombre et 1 caractère spécial."
+        // },
+    }
 });
 // !!!! WIP : vérifier si adresse mail valide comme pour p5???
 
